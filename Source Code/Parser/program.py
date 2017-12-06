40cdba560221607d1dcd0c29e36211711477bed3
Turcu Nicusor <nicusor.turcu@info.uaic.ro>
2017-12-06 22:17:14 +0200

    removed nltk lib, added install steps

--- Source Code/Parser/program.py
+++ Source Code/Parser/program.py
@@ -1,10 +1,3 @@
-import sys
-
-import os
-lib_path = os.path.join(os.path.abspath(os.path.join(os.getcwd() ,"..")),"Python Libraries")
-
-sys.path.append(lib_path)
-
 from nltk.tokenize import sent_tokenize, word_tokenize
 import utils as u
 
