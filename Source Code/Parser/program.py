5f35f03fbb9c21981bf12804d393be21c3da747e
TrifanTamara <trifantm@yahoo.com>
2017-12-09 22:26:14 +0200

    cum se foloseste taggset + stemmer + lematizer

--- Source Code/Parser/program.py
+++ Source Code/Parser/program.py
@@ -1,4 +1,6 @@
 from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
 import utils as u
 
 data, document = u.read_document()
@@ -6,3 +8,23 @@ words = word_tokenize(document)
 phrases = sent_tokenize(document)
 
 print(words, phrases)
+
+# word tokenize folosing limba romana
+print(pos_tag(word_tokenize(document), tagset='universal', lang='ro'))
+
+# exemplu de cum sa cauti ce inseamna un anumit tagset
+import nltk
+nltk.help.upenn_tagset('POS')
+
+#stemmer
+
+# http://snowball.tartarus.org/algorithms/romanian/stemmer.html
+from nltk.stem import SnowballStemmer
+lancaster_stemmmer = SnowballStemmer("romanian")
+print(lancaster_stemmmer.stem("abruptă"))
+
+# lematizer
+from nltk.stem import WordNetLemmatizer
+wordnet_lemmatizer = WordNetLemmatizer()
+print(wordnet_lemmatizer.lemmatize("churches"))
+# pentru romana e cu ?
