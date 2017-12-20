dcb46ec08e87ce4c77df98dd1affb3b75c603ceb
Turcu Nicusor <nicusor.turcu@info.uaic.ro>
2017-12-20 17:38:08 +0200

    commented unused inport in program

--- Source Code/Parser/program.py
+++ Source Code/Parser/program.py
@@ -1,16 +1,16 @@
 import utils as u
-from taggers import tagger_sync, tagger_async
-from tokenizers import tokenize, stem, lematizer
+# from taggers import tagger_sync, tagger_async
+# from tokenizers import tokenize, stem, lematizer
 import pos_tagger, np_chuncker, fdg_parser_ro, anaphora_resolution
 
 
-def apply_parser_v1(document):
-    sents = tokenize(document)
-    result = []
-    for sent in sents:
-        result.append(tagger_sync(sent))
-    # tagger_async(document, 8)
-    return result
+# def apply_parser_v1(document):
+#     sents = tokenize(document)
+#     result = []
+#     for sent in sents:
+#         result.append(tagger_sync(sent))
+#     # tagger_async(document, 8)
+#     return result
 
 def apply_parser_v2(document):
     data = fdg_parser_ro.parse_text(document)
@@ -19,5 +19,4 @@ def apply_parser_v2(document):
 if __name__ == '__main__':
     data, document = u.read_document()
     print(apply_parser_v2(document))
-    # print(apply_parser_v1(document))
 
